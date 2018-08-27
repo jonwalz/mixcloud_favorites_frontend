1157f64c6446335d44a9b3b916d97e6b8c0ab786
Jonathan Walz <jon.walz.music@gmail.com>
2018-08-26 21:48:57 -0700

    Added appolo client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import Aside from "./components/aside_container";
 import Contents from "./components/contents_container";
 import styled from "react-emotion";
+import { ApolloProvider } from "react-apollo";
+import client from "./client"
 import "./styles.css";
 
 const Container = styled("div")`
@@ -10,13 +12,32 @@ const Container = styled("div")`
   width: 100%;
  `;
 
-function App() {
-    return (
-        <Container>
-            <Aside>Aside</Aside>
-            <Contents>Content</Contents>
-        </Container>
-    );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            selectedDj: '',
+        }
+
+        this.setDj = this.setDj.bind(this)
+    }
+
+    setDj(selectedDj) {
+        this.setState({
+            selectedDj
+        })
+    }
+
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <Container>
+                    <Aside setDj={this.setDj} />
+                    <Contents selectedDj={this.state.selectedDj} />
+                </Container>
+            </ApolloProvider>
+        );
+    }
 }
 
 const rootElement = document.getElementById("root");
