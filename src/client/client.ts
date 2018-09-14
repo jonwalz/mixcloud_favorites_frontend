eb63d3c753ab50e2333164d1321096fbb41b12fa
Jon <jon.walz.music@gmail.com>
2018-09-14 13:19:49 -0700

    Fixed spelling error

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 import defaults from "./defaults"
 import resolvers from "./resolvers"
-import typeDefs from "./typedefs"
+import typeDefs from "./typeDefs"
 
 const client = new ApolloClient({
     uri: "https://mixcloud-favorites-hfenubcgct.now.sh",
