b2e75ca3f09dae274f94557e9f76344f7a519f92
Jon <jon.walz.music@gmail.com>
2018-09-21 17:37:10 -0700

    Formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,50 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Aside from "./components/aside_container";
-import Contents from "./components/contents_container";
-import styled from "react-emotion";
-import { ApolloProvider } from "react-apollo";
-import client from "./client/client"
-import "./styles.css";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Aside from './components/aside_container'
+import Contents from './components/contents_container'
+import styled from 'react-emotion'
+import { ApolloProvider } from 'react-apollo'
+import client from './client/client'
+import './styles.css'
 
-const Container = styled("div")`
+const Container = styled('div')`
   display: flex;
   width: 100%;
-`;
+`
 
-interface AppProps { }
+interface AppProps {}
 
 interface AppState {
-    readonly selectedDj: string
+	readonly selectedDj: string
 }
 
 class App extends React.Component<AppProps, AppState> {
-    constructor(props: AppProps) {
-        super(props)
-        this.state = {
-            selectedDj: '',
-        }
+	constructor(props: AppProps) {
+		super(props)
+		this.state = {
+			selectedDj: '',
+		}
 
-        this.setDj = this.setDj.bind(this)
-    }
+		this.setDj = this.setDj.bind(this)
+	}
 
-    setDj(selectedDj) {
-        this.setState({
-            selectedDj
-        })
-    }
+	setDj(selectedDj) {
+		this.setState({
+			selectedDj,
+		})
+	}
 
-    render() {
-        return (
-            <ApolloProvider client={client}>
-                <Container>
-                    <Aside setDj={this.setDj} />
-                    <Contents selectedDj={this.state.selectedDj} />
-                </Container>
-            </ApolloProvider>
-        );
-    }
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<Container>
+					<Aside setDj={this.setDj} />
+					<Contents selectedDj={this.state.selectedDj} />
+				</Container>
+			</ApolloProvider>
+		)
+	}
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
